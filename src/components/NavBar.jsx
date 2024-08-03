ed4a4d44639c0e94be1a093b9e30b7526a52515b
kayikalvin <kayikalvin@gmail.com>
2024-08-03 13:15:54 +0300

    creating the navbar components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 
+
+const links = [
+    {
+        id: 1,
+        link: 'home'
+    },
+    {
+        id: 2,
+        link: 'about'
+    },
+    {
+        id: 3,
+        link: 'portfolio'
+    },
+    {
+        id: 4,
+        link: 'experience'
+    },
+    {
+        id: 5,
+        link: 'contacts'
+    },
+    
+]
+
+
+
 const NavBar = () => {
   return (
-    <div className='flex justify-between items-center w-full h-20 text-white fixed'>
-        NavBar
-    
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white fixed bg-black'>
+        <h1 className='text-5xl font-signature ml-2'>Kalvoh</h1>
+
+        <ul className='flex'>
+            {links.map(({id,link}) => (
+             <li
+                key={id}
+                className='px-4 cursor-pointer capitalize font-medium text-gray-400 hover:scale-125 duration-200'>
+                {link}
+             </li>
+        ))}
+        </ul>
     </div>
   )
 }
