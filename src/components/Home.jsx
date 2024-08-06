26186e0a0ef10750693b65f0493bcfd163674a28
kayikalvin <kayikalvin@gmail.com>
2024-08-06 18:08:56 +0300

    color change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { GoArrowUpRight } from "react-icons/go";
 
 const Home = () => {
   return (
-    <div name ='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-50 md:pt-20'>
+    <div name ='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-cyan-950 pt-50 md:pt-20'>
         <div className='max-w-screen-lg mx-auto flex gap-40 flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center w-fullh-full'>
                 <h2 className='text-4xl sm:text-6xl font-bold text-white'>I'm a Data Analyst</h2>
@@ -24,7 +24,7 @@ const Home = () => {
                     React,Tailwind,Next js and GrapgQL.
                 </p>
                 <div>
-                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md shadow-xl bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                         Portfolio 
                         <span className='group-hover:rotate-90 duration-200'>
                             <GoArrowUpRight size={10} className='ml-1'/>
