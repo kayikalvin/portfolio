02728d95e62ad93f76afcce305b66cfe973333db
kayikalvin <kayikalvin@gmail.com>
2024-08-07 10:25:41 +0300

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'> About</p>
+
+            </div>
+            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nisi amet pariatur enim nam deleniti! Illum quam expedita alias repudiandae hic magni minima, laboriosam est tempore, maiores iusto dolor non qui tempora odit quasi eius? Rerum iste perferendis provident minima similique nulla qui, voluptatibus est delectus soluta ad aperiam! Dolorem.</p>
+            <br />
+            <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel fugiat voluptatem velit. Suscipit sapiente sequi animi repellat velit deleniti cumque ducimus voluptatum magni quisquam debitis soluta nesciunt aliquid architecto delectus nihil asperiores quam tempora natus impedit, dolore aspernatur. A at unde dolorem quaerat quidem, architecto facilis pariatur labore iste?</p>
+        </div>
+    </div>
   )
 }
 
