a928cdcb2e7a9228d1ed92fee64fce2781eee028
kayikalvin <kayikalvin@gmail.com>
2024-08-08 09:59:11 +0300

    few changes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,17 +8,23 @@ import reactWeather from "../assets/portfolio/reactWeather.jpg"
 
 const Portfolio = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b to-cyan-950 from-black text-white'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'> Portfolio</p>
-
+   <div name='portfolio'>
+    <div>
+        <div>
+            <p>Portfolio</p>
+            <p>Check out some of my work right now</p>
+        </div>
+        <div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <button>Demo</button>
+                    <button>Code</button>
+                </div>
             </div>
-            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nisi amet pariatur enim nam deleniti! Illum quam expedita alias repudiandae hic magni minima, laboriosam est tempore, maiores iusto dolor non qui tempora odit quasi eius? Rerum iste perferendis provident minima similique nulla qui, voluptatibus est delectus soluta ad aperiam! Dolorem.</p>
-            <br />
-            <p className='text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel fugiat voluptatem velit. Suscipit sapiente sequi animi repellat velit deleniti cumque ducimus voluptatum magni quisquam debitis soluta nesciunt aliquid architecto delectus nihil asperiores quam tempora natus impedit, dolore aspernatur. A at unde dolorem quaerat quidem, architecto facilis pariatur labore iste?</p>
         </div>
     </div>
+   </div>
   )
 }
 
