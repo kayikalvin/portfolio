0105af884260985127652d39a19bec08ecde0e29
kayikalvin <kayikalvin@gmail.com>
2024-08-14 23:35:01 +0300

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,72 +1,84 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import {Link} from 'react-scroll'
-
-
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 
 const NavBar = () => {
-const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(false);
+  const [activeLink, setActiveLink] = useState('home'); // State to track the active link
 
-const links = [
-    {
-        id: 1,
-        link: 'home'
+  const links = [
+    { id: 1,
+      link: 'home' 
     },
-    {
-        id: 2,
-        link: 'about'
+    { id: 2,
+       link: 'about' 
     },
-    {
-        id: 3,
-        link: 'portfolio'
+    { id: 3, 
+      link: 'portfolio'
     },
-    {
-        id: 4,
-        link: 'experience'
+    { id: 4,
+      link: 'experience' 
     },
-    {
-        id: 5,
-        link: 'contact'
+    { id: 5,
+      link: 'contact' 
     },
-  //comment 
-];
+  ];
+
+  // Function to handle link click and set active link
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    if (nav) setNav(false); // Close mobile menu on link click
+  };
+
   return (
     <div className='flex justify-between items-center w-full h-30 px-4 py-4 text-white fixed bg-black'>
-        <h1 className='text-6xl font-signature ml-2'>Kalvoh</h1>
-        {/* by default the navbar is hidden because tailwind is mobile first so for mobiles it has to be hidden */}
-        <ul className='hidden md:flex'>
-            {links.map(({id,link}) => (
-             <li key={id}
-                className='text-xl px-4 cursor-pointer capitalize font-semibold text-gray-400 hover:scale-105 duration-200 hover:text-cyan-500'>
-                <Link to={link} smooth duration={400}>{link}</Link> 
-             </li>
+      <h1 className='text-6xl font-signature ml-2'>Kalvoh</h1>
+      
+      {/* Desktop Menu */}
+      <ul className='hidden md:flex'>
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className={`text-xl px-4 cursor-pointer capitalize font-semibold ${
+              activeLink === link ? 'text-cyan-500' : 'text-gray-400'
+            } hover:scale-105 duration-200`}
+            onClick={() => handleLinkClick(link)}
+          >
+            <Link to={link} smooth duration={400} offset={-70}>
+              {link}
+            </Link>
+          </li>
         ))}
-        </ul>
+      </ul>
 
-        <div
+      {/* Mobile Menu Toggle */}
+      <div
         onClick={() => setNav(!nav)}
         className="cursor-pointer pr-4 md:hidden z-10 text-gray-500"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-    {/* nav && is used so that when it is true is when we can see the navbar */}
+
+      {/* Mobile Menu */}
       {nav && (
-        <ul className='flex flex-col justify-center  items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-        {links.map(({id,link}) => (
-            <li key = {id} 
-            className='px-4 cursor-pointer capitalize py-6 text-4xl'>
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className={`px-4 cursor-pointer capitalize py-6 text-4xl ${
+                activeLink === link ? 'text-cyan-500' : 'text-gray-400'
+              }`}
+              onClick={() => handleLinkClick(link)}
+            >
+              <Link to={link} smooth duration={400} offset={-70}>
                 {link}
+              </Link>
             </li>
-        ))}
-  </ul>
-
-
-
-
+          ))}
+        </ul>
       )}
-      
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
