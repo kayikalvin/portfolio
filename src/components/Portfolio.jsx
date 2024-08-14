9cbf98de24e956a0d373e3dcd92e78964f700aa5
kayikalvin <kayikalvin@gmail.com>
2024-08-14 13:40:27 +0300

    cuatomizing theme

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -57,7 +57,7 @@ const Portfolio = () => {
     };
 
     return (
-        <div name='portfolio' className='bg-gradient-to-b from-black to-gray-500 w-full text-white md:h-screen'>
+        <div name='portfolio' className='bg-gradient-to-b from-black to-gray-700 w-full text-white md:h-screen'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
@@ -66,7 +66,7 @@ const Portfolio = () => {
                 
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
                     {portfolios.map(({ id, src, demoUrl, codeUrl }) => (
-                        <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                        <div key={id} className='shadow-md shadow-cyan-800 rounded-lg'>
                             <img src={src} alt="" className='rounded-md duration-200 hover:scale-110' />
                             <div className='flex items-center justify-center'>
                                 <button 
